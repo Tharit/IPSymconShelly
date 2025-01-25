c13d2bb36e339272286fcc0957076507b4ab6202
Martin Kleinhans <kleinhans.martin@bcg.com>
2025-01-25 13:36:07 +0100

    added pm mini gen3

---
+++ ShellyPMMiniGen3/module.php
@@ -0,0 +1,63 @@
+<?php
+
+class ShellyPMMiniGen3 extends IPSModule
+{
+    public function Create()
+    {
+        //Never delete this line!
+        parent::Create();
+
+        $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
+
+        // properties
+        $this->RegisterPropertyString('Topic', '');
+
+        // variables
+        $this->RegisterVariableBoolean("Connected", "Connected");
+        $this->RegisterVariableFloat("PowerTotal", "PowerTotal", '~Electricity');
+        $this->RegisterVariableFloat("Power", "Power", '~Watt.3680');
+    }
+
+    public function ApplyChanges()
+    {
+        //Never delete this line!
+        parent::ApplyChanges();
+
+        $topic = $this->ReadPropertyString('Topic');
+        $this->SetReceiveDataFilter('.*' . $topic . '.*');
+    }
+
+    public function ReceiveData($JSONString)
+    {
+        $this->SendDebug('JSON', $JSONString, 0);
+        if (empty($this->ReadPropertyString('Topic'))) return;
+
+        $Buffer = json_decode($JSONString, true);
+
+        $Payload = $Buffer['Payload'];
+        if (array_key_exists('Topic', $Buffer)) {
+            if (fnmatch('*/online', $Buffer['Topic'])) {
+                $this->SetValue('Connected', $Payload == 'true');
+            }
+            if (fnmatch('*/events/rpc', $Buffer['Topic'])) {
+                $Payload = json_decode($Payload, true);
+                if (array_key_exists('params', $Payload)) {
+                    if (array_key_exists('pm1:0', $Payload['params'])) {
+                        if (array_key_exists('aenergy', $Payload['params']['aenergy'])) {
+                            $total = $Payload['params']['aenergy']['total'];
+
+                            $this->SetValue("PowerTotal", $total);
+                        }
+                        if (array_key_exists('apower', $Payload['params']['apower'])) {
+                            $this->SetValue('Power', $Payload['params']['apower']);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public function RequestAction($Ident, $Value)
+    {
+    }
+}
